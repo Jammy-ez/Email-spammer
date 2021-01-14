9a81cb504ca7ef309e18e1903a9c11af8ccebd6e
Jammy-ez <76751003+Jammy-ez@users.noreply.github.com>
2021-01-14 16:31:58 +0000

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -2,3 +2,7 @@ pip install tqdm
 pip install smtplib
 pip install colorama
 pip install pyfiglet
+py -m pip install pyfiglet
+py -m pip install colorama
+py -m pip install smtplib
+py -m pip install tqdm
